9c0a8f3a74794822d417a0b8f7ebfc8d001896b1
olivia-hsu-45 <b34121025@gs.ncku.edu.tw>
2026-07-11 18:55:41 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/hang-out-schedule/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
